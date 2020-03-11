3654c5ae7eb4e690d8d1a018afc2a78f61414a0a
Tulio Parreiras <tulio@usemobile.xyz>
2020-03-11 18:54:18 -0300

    Dispatch `CoreDataFeedStore` operations in a serial background queue to avoid blocking clients

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -31,50 +31,58 @@ class CoreDataFeedStore: FeedStore {
     }()
     
     func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        let context = CoreDataFeedStore().persistentContainer.viewContext
-        let fetchCache = NSFetchRequest<CoreDataCache>(entityName: "CoreDataCache")
-        let caches = try! context.fetch(fetchCache)
-        caches.forEach({
-            context.delete($0)
-        })
-        let fetchFeedImage = NSFetchRequest<CoreDataFeedImage>(entityName: "CoreDataFeedImage")
-        let feedImages = try! context.fetch(fetchFeedImage)
-        feedImages.forEach({
-            context.delete($0)
-        })
-        try! context.save()
-        completion(nil)
+        queue.async(flags: .barrier) { [weak self] in
+            guard let self = self else { return }
+            let context = self.persistentContainer.viewContext
+            let fetchCache = NSFetchRequest<CoreDataCache>(entityName: "CoreDataCache")
+            let caches = try! context.fetch(fetchCache)
+            caches.forEach({
+                context.delete($0)
+            })
+            let fetchFeedImage = NSFetchRequest<CoreDataFeedImage>(entityName: "CoreDataFeedImage")
+            let feedImages = try! context.fetch(fetchFeedImage)
+            feedImages.forEach({
+                context.delete($0)
+            })
+            try! context.save()
+            completion(nil)
+        }
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        
-        let context = persistentContainer.viewContext
-        let cache = NSEntityDescription.insertNewObject(forEntityName: "CoreDataCache", into: context) as! CoreDataCache
-        feed.forEach({
-            let coreDataFeedImage = NSEntityDescription.insertNewObject(forEntityName: "CoreDataFeedImage", into: context) as! CoreDataFeedImage
-            coreDataFeedImage.id = $0.id
-            coreDataFeedImage.imageDescription = $0.description
-            coreDataFeedImage.location = $0.location
-            coreDataFeedImage.url = $0.url
-            coreDataFeedImage.createdAt = $0.createdAt
-            cache.addToFeed(coreDataFeedImage)
-        })
-        cache.timestamp = timestamp
-        
-        try! context.save()
-        completion(nil)
+        queue.async(flags: .barrier) { [weak self] in
+            guard let self = self else { return }
+            let context = self.persistentContainer.viewContext
+            let cache = NSEntityDescription.insertNewObject(forEntityName: "CoreDataCache", into: context) as! CoreDataCache
+            feed.forEach({
+                let coreDataFeedImage = NSEntityDescription.insertNewObject(forEntityName: "CoreDataFeedImage", into: context) as! CoreDataFeedImage
+                coreDataFeedImage.id = $0.id
+                coreDataFeedImage.imageDescription = $0.description
+                coreDataFeedImage.location = $0.location
+                coreDataFeedImage.url = $0.url
+                coreDataFeedImage.createdAt = $0.createdAt
+                cache.addToFeed(coreDataFeedImage)
+            })
+            cache.timestamp = timestamp
+            
+            try! context.save()
+            completion(nil)
+        }
     }
     
     func retrieve(completion: @escaping RetrievalCompletion) {
-        let context = persistentContainer.viewContext
-        let fetchRequest = NSFetchRequest<CoreDataCache>(entityName: "CoreDataCache")
-        let fetchResponse = try! context.fetch(fetchRequest)
-        if let cache = fetchResponse.last {
-            let feed = cache.localFeed
-            let timestamp = cache.timestamp
-            completion(.found(feed: feed, timestamp: timestamp))
-        } else {
-            completion(.empty)
+        queue.async { [weak self] in
+            guard let self = self else { return }
+            let context = self.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<CoreDataCache>(entityName: "CoreDataCache")
+            let fetchResponse = try! context.fetch(fetchRequest)
+            if let cache = fetchResponse.last {
+                let feed = cache.localFeed
+                let timestamp = cache.timestamp
+                completion(.found(feed: feed, timestamp: timestamp))
+            } else {
+                completion(.empty)
+            }
         }
     }
     
@@ -117,92 +125,92 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
         
         self.undoStoreSideEffects()
     }
-	
-//
-//   We recommend you to implement one test at a time.
-//   Uncomment the test implementations one by one.
-// 	 Follow the process: Make the test pass, commit, and move to the next one.
-//
-
-	func test_retrieve_deliversEmptyOnEmptyCache() {
-		let sut = makeSUT()
-
-		assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
-	}
-
-	func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-		let sut = makeSUT()
-
-		assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
-	}
-
-	func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
-		let sut = makeSUT()
-
-		assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
-	}
-
-	func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
-		let sut = makeSUT()
-
-		assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
-	}
-
-	func test_insert_deliversNoErrorOnEmptyCache() {
-		let sut = makeSUT()
-
-		assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
-	}
-
-	func test_insert_deliversNoErrorOnNonEmptyCache() {
-		let sut = makeSUT()
-
-		assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
-	}
-
-	func test_insert_overridesPreviouslyInsertedCacheValues() {
-		let sut = makeSUT()
-
-		assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
-	}
-
-	func test_delete_deliversNoErrorOnEmptyCache() {
-		let sut = makeSUT()
-
-		assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
-	}
-
-	func test_delete_hasNoSideEffectsOnEmptyCache() {
-		let sut = makeSUT()
-
-		assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
-	}
-
-	func test_delete_deliversNoErrorOnNonEmptyCache() {
-		let sut = makeSUT()
-
-		assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
-	}
-
-	func test_delete_emptiesPreviouslyInsertedCache() {
-		let sut = makeSUT()
-
-		assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
-	}
-
-	func test_storeSideEffects_runSerially() {
-		let sut = makeSUT()
-
-		assertThatSideEffectsRunSerially(on: sut)
-	}
-	
-	// - MARK: Helpers
-	
-	private func makeSUT() -> FeedStore {
+    
+    //
+    //   We recommend you to implement one test at a time.
+    //   Uncomment the test implementations one by one.
+    // 	 Follow the process: Make the test pass, commit, and move to the next one.
+    //
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        let sut = makeSUT()
+        
+        assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        let sut = makeSUT()
+        
+        assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
+        
+        assertThatSideEffectsRunSerially(on: sut)
+    }
+    
+    // - MARK: Helpers
+    
+    private func makeSUT() -> FeedStore {
         let sut = CoreDataFeedStore()
         return sut
-	}
-	
+    }
+    
 }
 
 //
